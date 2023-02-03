2999048b70148c5e818fcd95df5ac8f5635b97f6
marypaduano <maria.paduano@gmail.com>
2023-02-03 17:52:38 +0100

    esercizio mail da correggere

---
+++ js/app.js
@@ -0,0 +1,24 @@
+console.log('work')
+
+let message = ''
+let inputMail
+//creare un array che contiene lista di email
+const mailList = ['mariapaduano@boolean.it','gianlucalomarco@boolean.it','giuseppefunicello@boolean.it','emanueleparatore@boolean.it']
+console.log(mailList)
+console.log(mailList.length)
+//chiedere all'utente di inserire una mail
+inputMail = prompt('Inserisci la tua mail')
+let emailValue = inputMail;
+console.log(emailValue)
+//controlla che la mail sia presente nella lista
+for (let i = 0; i < mailList.length; i++ ){
+    let access = mailList[i]
+    if (access === emailValue){
+        messagge = 'mail presente'        
+    }
+    else { message = 'mail non presente'}    
+}
+
+console.log(message)
+
+//stampa un messaggio di verifica
